c2562ada37af3556fa2a5e92731d92c5a868711e
ADITHYAN2004 <adhiyotu@gmail.com>
2025-05-11 22:55:21 +0530

    initial commit

---
+++ todo/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from todo.routes import user, todo
+from todo import models, database
+
+models.Base.metadata.create_all(bind=database.engine)
+
+app = FastAPI()
+
+
+
+app.include_router(user.router)
+app.include_router(todo.router)
+
+
